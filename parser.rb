5ac98ae212608d428f3617e8e1d651184b8d6f85
Tom Nullet <Tom@Toms-MacBook-Pro.local>
2014-05-02 15:55:53 -0500

    better parsing solution

--- parser.rb
+++ parser.rb
@@ -12,20 +12,20 @@
 		$array = []
 
 		File.open('flashcard_samples.txt', 'r').each do |line|
-			$array << line
+			$array << line.chomp unless line.chomp.empty?
 		end
 	end
 
 load_cards
-p $array
-$array.delete_if { |item| item == "\n"}
-p $array
-$array.map! do |item|
-	if item != $array.last
-		item = item[0..-2]
-	else
-		item
-	end
-end
+# p $array
+# $array.delete_if { |item| item == "\n"}
+# p $array
+# $array.map! do |item|
+# 	if item != $array.last
+# 		item = item[0..-1]
+# 	else
+# 		item
+# 	end
+# end
 
 puts $array
